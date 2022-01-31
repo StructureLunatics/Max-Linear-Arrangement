409f8f62270762d2bfda95395ab6c39542e7bc6b
dabadav <dante.avino@alum.esci.upf.edu>
2022-01-31 19:09:27 +0100

    Updating MakeFIle, declaring program 4 & 5

---
+++ code/program4.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <numeric>
+#include <vector>
+#include <iostream>
+#include "graph.hpp"
+#include "graph_utilities.hpp"
+
+using namespace std;
+
+/*  PROGRAM 4:
+
+    Input: various graphs
+    Output: the value of Dmax for each of them. 
+
+The results has to be calculated using a recursive algorithm for the exhaustive exploration. 
+*/
+
+int main()
+{
+    return 0;
+}
---
+++ code/program5.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <numeric>
+#include <vector>
+#include <iostream>
+#include "graph.hpp"
+#include "graph_utilities.hpp"
+
+using namespace std;
+
+/*  PROGRAM 5:
+
+    Input: various graphs
+    Output: the value of Dmax for each of them. 
+
+Variant of the previous program where the results are obtained applying a branch & bound technique over the implicit tree.
+*/
+
+int main()
+{
+    return 0;
+}
