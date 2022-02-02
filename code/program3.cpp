3bc9f362eb6cc6b9f8203c6e1d6cb0a8ff8a1382
dantekali <dante.avino@alum.esci.upf.edu>
2022-02-02 13:08:48 +0100

    Testing

--- code/program3.cpp
+++ code/program3.cpp
@@ -2,6 +2,10 @@
 #include <numeric>
 #include <vector>
 #include <iostream>
+
+
+
+#include <algorithm>
 #include "graph.hpp"
 #include "graph_utilities.hpp"
 
@@ -15,7 +19,37 @@ using namespace std;
 The results has to be calculated using an exhaustive enumeration of the n! linear arrangements. 
 */
 
-int main()
-{
+int main(){
+    // Storing number of graphs we will have to read.
+    int totalG;
+    cin >> totalG;
+
+    // Initializing a vector to store the Ds
+    vector<int> Dmax_v;
+
+    int myints[] = {1,2,3};
+
+    std::sort (myints,myints+3);
+
+    std::cout << "The 3! possible permutations with 3 elements:\n";
+    do {
+    std::cout << myints[0] << ' ' << myints[1] << ' ' << myints[2] << '\n';
+    } while ( std::next_permutation(myints,myints+3) );
+
+    std::cout << "After loop: " << myints[0] << ' ' << myints[1] << ' ' << myints[2] << '\n';
+
+
+    for(int i = 0; i < totalG; i++){
+        // Declare graph object
+        graph g;
+        // Call ReadGraph function
+        g = ReadGraph();
+
+        // Permutate all possible positions
+
+        // Save value from permutation of highest Dmax
+
+    }
+
     return 0;
 }
