61c64653c95259e18a427ff8dacf3bdb640a75c7
dabadav <dante.avino@alum.esci.upf.edu>
2022-02-03 11:10:15 +0100

    og2

--- code/program3.cpp
+++ code/program3.cpp
@@ -33,17 +33,17 @@ int main(){
         vector <int> pi(g.vertices());
         iota(pi.begin(), pi.end(), 0);
 
-        vector<int> AllTempD;
+        vector<int> AlltmpD;
         // Permutate all possible positions
         do {
             // Call compute D taking two params (vertices) method
             int D = ComputeD(g, pi);
             // Insert the D value inside Dvalues vector
-            AllTempD.push_back(D);
+            AlltmpD.push_back(D);
         } while ( next_permutation(pi.begin(), pi.end()) );
 
         // Find value from permutation of highest distance
-        int MaxD = *max_element (AllTempD.begin(), AllTempD.end());
+        int MaxD = *max_element (AlltmpD.begin(), AlltmpD.end());
 
         // Save value from permutation of highest distance
         AllD.push_back(MaxD);
